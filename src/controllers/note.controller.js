fdaf9021fac6a51692da6ff5dc95d5663dde02a6
Devanshi-Vadiya <devanshivadiya@gmail.com>
2026-04-18 10:12:13 +0530

    3

--- src/controllers/note.controller.js
+++ src/controllers/note.controller.js
@@ -52,3 +52,18 @@ exports.createNotesBulk = async (req, res) => {
     res.status(500).json({ success: false, message: err.message, data: null });
   }
 };
+
+// 3. Get all
+exports.getAllNotes = async (req, res) => {
+  try {
+    const notes = await Note.find();
+
+    res.status(200).json({
+      success: true,
+      message: "Notes fetched successfully",
+      data: notes,
+    });
+  } catch (err) {
+    res.status(500).json({ success: false, message: err.message, data: null });
+  }
+};
--- src/routes/note.routes.js
+++ src/routes/note.routes.js
@@ -5,5 +5,6 @@ const controller = require("../controllers/note.controller");
 
 router.post("/", controller.createNote);
 router.post("/bulk", controller.createNotesBulk);
+router.get("/", controller.getAllNotes);
 
 module.exports = router;
