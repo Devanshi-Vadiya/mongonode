2cc0de99f8376a28f1381cb92b3f9f8f823e330c
Devanshi-Vadiya <devanshivadiya@gmail.com>
2026-04-18 10:13:03 +0530

    4

--- src/controllers/note.controller.js
+++ src/controllers/note.controller.js
@@ -67,3 +67,36 @@ exports.getAllNotes = async (req, res) => {
     res.status(500).json({ success: false, message: err.message, data: null });
   }
 };
+
+// 4. Get by ID
+exports.getNoteById = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    if (!isValidId(id)) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid note ID",
+        data: null,
+      });
+    }
+
+    const note = await Note.findById(id);
+
+    if (!note) {
+      return res.status(404).json({
+        success: false,
+        message: "Note not found",
+        data: null,
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "Note fetched successfully",
+      data: note,
+    });
+  } catch (err) {
+    res.status(500).json({ success: false, message: err.message, data: null });
+  }
+};
--- src/routes/note.routes.js
+++ src/routes/note.routes.js
@@ -6,5 +6,6 @@ const controller = require("../controllers/note.controller");
 router.post("/", controller.createNote);
 router.post("/bulk", controller.createNotesBulk);
 router.get("/", controller.getAllNotes);
+router.get("/:id", controller.getNoteById);
 
 module.exports = router;
