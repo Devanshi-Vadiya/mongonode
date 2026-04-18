459e3ebd340142461264fabb32342187102fbcbf
Devanshi-Vadiya <devanshivadiya@gmail.com>
2026-04-18 10:20:06 +0530

    6

--- src/routes/note.routes.js
+++ src/routes/note.routes.js
@@ -9,5 +9,5 @@ router.get("/", controller.getAllNotes);
 router.get("/:id", controller.getNoteById);
 
 router.put("/:id", controller.replaceNote);
-
+router.patch("/:id", controller.updateNote);
 module.exports = router;
